11cc0eb5cb6598646323e3acc2e23a233a6d3815
Jiamu Chai <j.chai@fetchrewards.com>
2025-06-12 11:30:09 -0500

    initital commit

---
+++ app/src/main/java/com/fetch/fetchit/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.fetch.fetchit
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.fetch.fetchit.R
+import com.fetch.fetchit.ui.theme.FetchitTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FetchitTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.mipmap.grocerystore_foreground),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FetchitTheme {
+        Greeting("Android")
+    }
+}
